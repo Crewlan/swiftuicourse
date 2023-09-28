5b532c50687102fafd59f285282fc344901046fe
Rodrigo Silva <rodrigoa.work@gmail.com>
2023-09-28 20:04:17 -0300

    Initial Commit

---
+++ swiftuicourse/swiftuicourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuicourseApp.swift
+//  swiftuicourse
+//
+//  Created by Rodrigo Silva on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuicourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
